b0fc04917c3fc214856ef1c750a002c0394f8e44
Arwa <arwafs1@gmail.com>
2022-03-22 21:25:46 -0500

    set up my selenium project

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.sel.test;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
